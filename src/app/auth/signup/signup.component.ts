e1e623b60f03811b3145b7ac4279a17b366aa321
nave <navegit@gmail.com>
2018-05-24 22:09:47 +0300

    start signup form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  signupForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null)
+    })
   }
-
+  onSignupSubmit() {
+    console.log(this.signupForm)
+  }
 }
