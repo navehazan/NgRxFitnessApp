6e10c188ee8822fa808b5e912a6717a1ee56174f
nave <navegit@gmail.com>
2018-05-31 21:44:13 +0300

    fix our team component on mobile

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { InfoService } from '../../services/info-service';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from '../current-training/stop-training/stop-training.component';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, public router: Router) { }
   progress = 0;
   timer;
   ngOnInit() {
@@ -20,9 +21,17 @@ export class CurrentTrainingComponent implements OnInit {
     }, 1000);
   }
   stopExercise() {
-    clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent, {
+
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
       data: { progress: this.progress },
     });
+    dialogRef.afterClosed().subscribe(resultes => {
+      if (resultes) {
+        clearInterval(this.timer);
+        this.router.navigate(["/"])
+      } else {
+        this.dialog.closeAll();
+      }
+    })
   }
 }
--- src/app/training/current-training/stop-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training/stop-training.component.ts
@@ -10,7 +10,7 @@ export class StopTrainingComponent implements OnInit {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
-    console.log(this.data)
+
   }
 
 }
