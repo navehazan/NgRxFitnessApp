89239234004cf33e9e2b8db228cb191128e01297
nave <navegit@gmail.com>
2018-06-03 00:09:55 +0300

    fix our team component on mobile

--- src/app/components/training/past-trainings/past-trainings.component.ts
+++ src/app/components/training/past-trainings/past-trainings.component.ts
@@ -1,21 +1,28 @@
 import { Exercise } from './../../../models/exercise.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { TrainingService } from '../../../services/training.service';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(private trainingService: TrainingService) { }
   dataSource = new MatTableDataSource<Exercise>();
   displayedColumns = ["date", "name", "calories", "duration", "state"];
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getPastExercise();
-    console.log(this.dataSource.data)
   }
-
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  doFilter(filter: string) {
+    this.dataSource.filter = filter.trim().toLocaleLowerCase();
+  }
 }
