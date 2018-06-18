efb75315a034563091011be2564ccddd0629940f
nave <navegit@gmail.com>
2018-06-18 23:52:09 +0300

    dd

--- src/app/components/training/new-training/new-training.component.ts
+++ src/app/components/training/new-training/new-training.component.ts
@@ -4,7 +4,7 @@ import { InfoService } from '../../../services/info-service';
 import { TrainingService } from '../../../services/training.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AngularFirestore } from "angularfire2/firestore";
-
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
@@ -20,8 +20,15 @@ export class NewTrainingComponent implements OnInit {
     this.newTrainingForm = new FormGroup({
       exercise: new FormControl(null)
     })
-    this.db.collection("aviableExersice").valueChanges().subscribe(results => {
-      console.log(results)
+    this.db.collection("aviableExersice").snapshotChanges().map((results) => {
+      return results.map((item) => {
+        return {
+          id: item.payload.doc.id,
+          ...item.payload.doc.data()
+        }
+      })
+    }).subscribe((results: Exercise[]) => {
+      this.availableExercise = results;
     })
   }
   onGoingTraining() {
