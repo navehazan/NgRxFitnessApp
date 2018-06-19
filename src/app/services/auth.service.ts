f66d7680cc12fb4f8a1d2f40e3b3cbc36a28fab3
nave <navegit@gmail.com>
2018-06-19 20:51:36 +0300

    dd

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,44 +7,39 @@ import { AngularFireAuth } from "angularfire2/auth";
 @Injectable()
 export class AuthService {
     constructor(private router: Router, private afAuth: AngularFireAuth) { }
-    private user: User;
+    isAuthenticated = false;
     isLogin = new Subject<boolean>();
     registerUser(authData: AuthData) {
-        this.user = {
-            email: authData.email, userId: Math.round(Math.random() * 1000)
-        };
         this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password).then((res) => {
             console.log(res)
+            this.authSuccessfully();
         }).catch((err) => {
             console.log(err)
         })
-        this.authSuccessfully();
+
     }
     login(authData: AuthData) {
-        this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password).then((res) => {
+        this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password).then((res) => {
             console.log(res)
+            this.authSuccessfully();
         }).catch((err) => {
             console.log(err)
         })
-        this.user = {
-            email: authData.email, userId: Math.round(Math.random() * 1000)
-        };
-        this.authSuccessfully();
+
     }
     logut() {
-        this.user = null;
+        this.isAuthenticated = false;
         this.isLogin.next(false);
         this.router.navigate(["/login"])
     }
-    getUser() {
-        return { ...this.user };
-    }
+
 
     authSuccessfully() {
+        this.isAuthenticated = true;
         this.isLogin.next(true);
         this.router.navigate(["/training"]);
     }
     isAuth() {
-        return this.user != null;
+        return this.isAuthenticated;
     }
 }
